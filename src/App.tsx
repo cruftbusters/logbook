81a232734c6be858b3896ddf7b3e2f7d9357723e
tyler <tyler@cruftbusters.com>
2025-11-21 07:25:48 +0000

    refactor to extract navigator from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-import { Logbook } from './types'
-import { useLogbooks } from './useLogbooks'
+import { Navigator } from './Navigator'
 import './App.css'
 
-function App() {
+export function App() {
   return (
     <>
       <header>
@@ -13,93 +11,6 @@ function App() {
         <Navigator />
       </section>
     </>
-  )
-}
-
-function Navigator() {
-  const { list, status, ...actions } = useLogbooks()
-
-  return (
-    <p
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        gridGap: '1em',
-      }}
-    >
-      {list.items.map((logbook) => (
-        <ListItem
-          key={logbook.id}
-          logbook={logbook}
-          ondelete={() => actions.delete(logbook.id)}
-          onrename={(title) => actions.rename(logbook.id, title)}
-        />
-      ))}
-      <button onClick={actions.create}>create logbook</button>
-      <button hidden={list.items.length < 1} onClick={actions.clear}>
-        clear logbooks
-      </button>
-      <span role="status">{status}</span>
-    </p>
-  )
-}
-
-function ListItem({
-  logbook,
-  ondelete,
-  onrename,
-}: {
-  logbook: Logbook
-  ondelete: () => void
-  onrename: (title: string) => void
-}) {
-  const [isTitleEdit, setTitleEdit] = useState(false)
-  const [title, setTitle] = useState(logbook.title)
-
-  return (
-    <li
-      style={{
-        display: 'inherit',
-        gridGap: '1em',
-      }}
-    >
-      <span className="like-button" style={{ flex: '1' }} hidden={isTitleEdit}>
-        {logbook.title}
-      </span>
-      <button
-        hidden={isTitleEdit}
-        onClick={() => {
-          setTitleEdit(true)
-        }}
-      >
-        rename
-      </button>
-      <input
-        aria-label="name"
-        hidden={!isTitleEdit}
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-      />
-      <button
-        hidden={!isTitleEdit}
-        onClick={() => {
-          onrename(title)
-          setTitleEdit(false)
-        }}
-      >
-        accept
-      </button>
-      <button
-        hidden={!isTitleEdit}
-        onClick={() => {
-          setTitle(logbook.title)
-          setTitleEdit(false)
-        }}
-      >
-        discard
-      </button>
-      <button onClick={ondelete}>delete</button>
-    </li>
   )
 }
 
---
+++ src/Navigator.tsx
@@ -0,0 +1,90 @@
+import { useState } from 'react'
+import { Logbook } from './types'
+import { useLogbooks } from './useLogbooks'
+
+export function Navigator() {
+  const { list, status, ...actions } = useLogbooks()
+
+  return (
+    <p
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gridGap: '1em',
+      }}
+    >
+      {list.items.map((logbook) => (
+        <ListItem
+          key={logbook.id}
+          logbook={logbook}
+          ondelete={() => actions.delete(logbook.id)}
+          onrename={(title) => actions.rename(logbook.id, title)}
+        />
+      ))}
+      <button onClick={actions.create}>create logbook</button>
+      <button hidden={list.items.length < 1} onClick={actions.clear}>
+        clear logbooks
+      </button>
+      <span role="status">{status}</span>
+    </p>
+  )
+}
+
+function ListItem({
+  logbook,
+  ondelete,
+  onrename,
+}: {
+  logbook: Logbook
+  ondelete: () => void
+  onrename: (title: string) => void
+}) {
+  const [isTitleEdit, setTitleEdit] = useState(false)
+  const [title, setTitle] = useState(logbook.title)
+
+  return (
+    <li
+      style={{
+        display: 'inherit',
+        gridGap: '1em',
+      }}
+    >
+      <span className="like-button" style={{ flex: '1' }} hidden={isTitleEdit}>
+        {logbook.title}
+      </span>
+      <button
+        hidden={isTitleEdit}
+        onClick={() => {
+          setTitleEdit(true)
+        }}
+      >
+        rename
+      </button>
+      <input
+        aria-label="name"
+        hidden={!isTitleEdit}
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <button
+        hidden={!isTitleEdit}
+        onClick={() => {
+          onrename(title)
+          setTitleEdit(false)
+        }}
+      >
+        accept
+      </button>
+      <button
+        hidden={!isTitleEdit}
+        onClick={() => {
+          setTitle(logbook.title)
+          setTitleEdit(false)
+        }}
+      >
+        discard
+      </button>
+      <button onClick={ondelete}>delete</button>
+    </li>
+  )
+}
